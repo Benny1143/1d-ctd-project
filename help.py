1c252463e0b57755781cea154f663e5c6025c54b
Eugene <eugeneoon2001@gmail.com>
2022-12-01 17:14:32 +0800

    check_win function and count_points function

--- help.py
+++ help.py
@@ -42,5 +42,57 @@ def readFile(stage):
 
 
 
-def hello(char, coord, all_charwin_cond):
+def check_win(char, coord, all_char, win_cond):
+    #char: str character moved
+    #coord: tuple coordinate of character moved
+    #all_char: dictionary of all characters with coordinates
+    #win_cond: dictionary of key object
+    
+    # unpack coord tuple
+    x, y = coord
+    
+    # compile list of words
+    ls = []
+
+    # check left of char
+    if all_char.get((x-1, y)) != None:
+        left_char = all_char.get((x-1, y))
+        ls.append(left_char + char)
+    # check top of char
+    if all_char.get((x, y+1)) != None:
+        top_char = all_char.get((x, y+1))
+        ls.append(top_char + char)
+    # check right of char
+    if all_char.get((x+1, y)) != None:
+        right_char = all_char.get((x+1, y))
+        ls.append(char + right_char)
+    # check bottom of char
+    if all_char.get((x, y-1)) != None:
+        bottom_char = all_char.get((x, y-1))
+        ls.append(char + bottom_char)
+    
+    # call function to count points
+    return count_points(ls, win_cond)
     pass
+
+def count_points(ls, win_cond):
+    #ls: list of phrase that exist
+    #win_cond: dictionary of of key object
+    
+    # initialise total points
+    total_points = 0
+
+    # iterate over the ls of possible phrases
+    for i in ls:
+        if i in win_cond:
+            # total_points += win_cond[i].point
+            # When wc is not matched
+            if win_cond.won is False:
+                win_cond.matched()
+                total_point += win_cond[i].point
+    return total_points
+
+
+
+
+#check_win("c", (1,1), {(1,2): "d", (2,1): "e"}, {})
