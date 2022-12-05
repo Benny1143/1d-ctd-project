9cf87423a2bf1923f896584f60161e7ac2ef4d52
benny <bennyrobot1143@gmail.com>
2022-12-06 01:43:14 +0800

    first edit of filesystem file

--- filesystem.py
+++ filesystem.py
@@ -1,4 +1,4 @@
-class winningCondition:
+class WinningCondition:
 
     def __init__(self, name, point):
         self.name = name
@@ -10,77 +10,75 @@ class winningCondition:
     def point(self) -> str:
         return self.point
 
-def get_character(s):
+
+def get_character(string: str):
+    #  string example: "我1,1;马2,2"
     characters = {}
     user = ()
-    for e in s.split(";"):
+    for e in string.split(";"):
         char = e[0]
-        coor = e[1:].split(",")
-        x =  int(coor[0])
-        y = int(coor[1])
-        characters[(x,y)] = char
+        x, y = e[1:].split(",")
+        x = int(x)
+        y = int(y)
+        characters[(x, y)] = char
         if "我" == char:
-            user = (x,y)
+            user = (x, y)
+    #  TODO: Print Error if User is not found
     return characters, user
 
-def get_winnings(t):
-    winningConditions = {}
-    for i in t.split(";"):
-        a = i[:].split(",")
-        winchar = str (a[0])
-        name =  str (a[1])
-        point = (a[2])
-        winningConditions[winchar] = winningCondition(name, point)
-    return winningConditions
 
-def getMapInfo(mapID) -> tuple[dict, dict, tuple]:
-    filename = str(mapID) + ".txt"
-    
-    f = open(filename, "r")
-    lines = f.readlines()
-        # Read file then
-        # return get_character from the string in the file
+def get_winnings(string: str):
+    # string example: "马蹄,chestnut,4;马路,road,2"
+    winning_conditions = {}
+    for i in string.split(";"):
+        a = i.split(",")
+        winning_character = str(a[0])
+        name = str(a[1])
+        point = int(a[2])
+        winning_conditions[winning_character] = WinningCondition(name, point)
+    return winning_conditions
 
-def ConvDictToString(characters, winningConditons):
+
+def getMapInfo(mapID: str) -> tuple[dict, dict, tuple]:
+    # Pesudo Values
+    # user = (3, 5)
+    # characters = {(3, 5): "我", (3, 4): "马"}
+    # winningConditions = {
+    #     "马蹄": winningCondition("hoof", 4),
+    #     "马路": winningCondition("road", 2)
+    # }
+    # return (characters.copy(), winningConditions.copy(), user)
+    with open(str(mapID) + ".txt", "r", encoding="utf8") as f:
+        lines = f.read()
+        (characterstring, winningstring) = lines.split("\n")
+        characters, user = get_character(characterstring)
+        winning_conditions = get_winnings(winningstring)
+        return characters.copy(), winning_conditions.copy(), user
+
+
+def dict_to_string(characters: dict[tuple[int, int], str], winning_conditions: dict[str, WinningCondition]):
     convertedchar = ""
     convertedwin = ""
     for charkey in characters:
         convertedchar += f"{characters[charkey]}{charkey[0]},{charkey[1]};"
 
-    for winkey in winningConditons:
-        convertedwin += f"{winkey},{winningConditons[winkey].name},{winningConditons[winkey].point};"
+    for winkey in winning_conditions:
+        convertedwin += f"{winkey},{winning_conditions[winkey].name},{winning_conditions[winkey].point};"
     return convertedchar[:-1] + "\n" + convertedwin[:-1]
-   
-
-def writeToFile(filename, cdic, wcdic):
-    def ConvDictToString(characters, winningConditons):
-
-        convertedchar = ""
-        convertedwin = ""
-       
-        for charkey in characters:
-            convertedchar += f"{characters[charkey]}{charkey[0]},{charkey[1]};"
-
-        for winkey in winningConditons:
-            convertedwin += f"{winkey},{winningConditons[winkey].name},{winningConditons[winkey].point};"
-        return convertedchar[:-1] + "\n" + convertedwin[:-1]
-    # Test case
-    print(ConvDictToString({(3, 5): "我", (3, 4): "马"}, {
-        "马蹄": winningCondition("hoof", 4),
-        "马路": winningCondition("road", 2)
-    }))
 
 
-    # Pesudo Values
-    user = (3, 5)
+def write_map_to_file(map_id: str, characters: dict[tuple[int, int], str], winning_conditions: dict[str, WinningCondition]):
+    data = dict_to_string(characters, winning_conditions)
+    with open(str(map_id) + ".txt", "w", encoding="utf8") as f:
+        f.write(data)
+
+
+if __name__ == "__main__":
+    # Write Test Cases
     characters = {(3, 5): "我", (3, 4): "马"}
-    # characters_get((3,4)) if return None, no character is at this position
-    winningConditions = {
-        "马蹄": winningCondition("hoof", 4),
-        "马路": winningCondition("road", 2)
+    winning_conditions = {
+        "马蹄": WinningCondition("hoof", 4),
+        "马路": WinningCondition("road", 2)
     }
-    #store the winning condition as string "马蹄,chestnut,4;马路,road,2"
-    #convert to a dict {"马蹄": {name: "chestnut", point: 4}, "马路": {name: "road", point: 2}} 
-    return (characters.copy(), winningConditions.copy(), user)
-
-# writeToFile(1)
+    print(getMapInfo(1))
+    print(write_map_to_file(1, characters, winning_conditions))
--- filetest.py
+++ filetest.py
@@ -1,4 +1,4 @@
-from filesystem import get_character, ConvDictToString, get_winnings
+from filesystem import get_character, dict_to_string, get_winnings
 f = open("1.txt", "r", encoding="utf8")
 lines = f.read()
 (characterstring, winningstring) = lines.split("\n")
@@ -7,5 +7,5 @@ characterDic, user = get_character(characterstring)
 winningDic = get_winnings(winningstring)
 
 f = open("1.txt", "w", encoding="utf8" )
-f.write(ConvDictToString(characterDic, winningDic))
+f.write(dict_to_string(characterDic, winningDic))
 f.close()
